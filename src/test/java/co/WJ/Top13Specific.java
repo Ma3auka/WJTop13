d406907f67fd733107cc35c3dbe9f504b66d2956
Artem Pavelko <ma3au.pav@gmail.com>
2019-07-11 12:51:35 +0300

    update pom / maven

--- src/test/java/co/WJ/Top13Specific.java
+++ src/test/java/co/WJ/Top13Specific.java
@@ -4,15 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
-import org.junit.rules.Timeout;
-import org.w3c.dom.Text;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Sleeper;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.apache.commons.exec.environment.DefaultProcessingEnvironment;
-import org.checkerframework.checker.formatter.FormatUtil;
-import java.util.concurrent.TimeUnit;
 
 public class Top13Specific extends WebDriver {
 
@@ -21,20 +12,17 @@ public class Top13Specific extends WebDriver {
         driver.get("https://zales.wonderjewel.co/configurator/XC2192K");
         System.out.println("- Open url");
 
+        new Select(driver.findElement(By.id("diamond-size"))).selectByVisibleText("1/2 CT."); //drop-down list
+        System.out.println("- Click 1/2");
+
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Build your own ring"));
         System.out.println("- Check title");
 
-        new Select(driver.findElement(By.id("diamond-size"))).selectByVisibleText("1/2 CT."); //drop-down list
-        System.out.println("- Click 1/2");
+//      Assert.assertTrue(driver.findElement(By.className(".builder-price-title sticky-content sticky-block")).getText().contains("$3,699.99"));
+//      System.out.println("- Cost Done");
 
-
-
-
-
-
-        Assert.assertTrue(driver.findElement(By.cssSelector(".builder-price-block")).getText().contains("3,699.99"));
-        System.out.println("- Cost Done");
     }
+
 }
 
