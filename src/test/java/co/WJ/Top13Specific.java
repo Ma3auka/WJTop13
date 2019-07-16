84fa50c837a2d11a2a0d08e35ee272dd439d45d1
Artem Pavelko <ma3au.pav@gmail.com>
2019-07-16 22:39:50 +0300

    Weit

--- src/test/java/co/WJ/Top13Specific.java
+++ src/test/java/co/WJ/Top13Specific.java
@@ -4,19 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class Top13Specific extends WebDriver {
 
     @Test
     public void Top13() throws Exception { //Открыть ссылку и проверка на title
+
+        WebDriverWait wait = new WebDriverWait(driver, 30, 500); //Ожидание элемента
+
         driver.get("https://zales.wonderjewel.co/configurator/XC2192K");
         System.out.println("- Open url");
 
         new Select(driver.findElement(By.id("diamond-size"))).selectByVisibleText("1/2 CT."); //drop-down list
         System.out.println("- Click 1/2");
 
-        Thread.sleep(10000);
-        
+        //Thread.sleep(10000);
+
+       // Assert.assertTrue(driver.findElement());
 
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Build your own ring"));
