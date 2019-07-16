aa5e56dab55a4cba003dc95ba811371e06e65336
Artem Pavelko <ma3au.pav@gmail.com>
2019-07-16 22:18:11 +0300

    Sleep

--- src/test/java/co/WJ/Top13Specific.java
+++ src/test/java/co/WJ/Top13Specific.java
@@ -8,12 +8,15 @@ import org.openqa.selenium.support.ui.Select;
 public class Top13Specific extends WebDriver {
 
     @Test
-    public void Top13() { //Открыть ссылку и проверка на title
+    public void Top13() throws Exception { //Открыть ссылку и проверка на title
         driver.get("https://zales.wonderjewel.co/configurator/XC2192K");
         System.out.println("- Open url");
 
         new Select(driver.findElement(By.id("diamond-size"))).selectByVisibleText("1/2 CT."); //drop-down list
         System.out.println("- Click 1/2");
+
+        Thread.sleep(10000);
+        
 
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Build your own ring"));
