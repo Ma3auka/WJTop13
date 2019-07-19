7fae46f24ad2a00f35472e6300f38b2b89531530
Artem Pavelko <ma3au.pav@gmail.com>
2019-07-19 18:18:44 +0300

    click RD / Version Br

--- src/test/java/co/WJ/Top13Specific.java
+++ src/test/java/co/WJ/Top13Specific.java
@@ -1,53 +1,127 @@
 package co.WJ;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class Top13Specific extends WebDriver {
 
-
     @Test
-    public void Top13_XC2192K_MQ_12() throws Exception { //Открыть ссылку и проверка на title
+    public void Top13_XC2192K_MQ_12() throws Exception {
 
         driver.get("https://zales.wonderjewel.co/configurator/XC2192K");
         System.out.println("- Open url");
 
-        if (driver.getPageSource().contains("2,699.77")) {
-            System.out.println("Cost Done");
-        }
-        else {
-            System.out.println("-Cost Fail");
-        }
-
-        Thread.sleep(2000);
-
-
-        driver.findElement(By.cssSelector("Body")).sendKeys(Keys.CONTROL + "t"); // открыть вкладку в новом окне
-    }
-
-
-    @Test
-    public void Top13_XC2192K_OV_12() throws Exception {
-
-        driver.get("https://zales.wonderjewel.co/configurator/XC2192K");
-        System.out.println("- Open url");
+        String title = driver.getTitle();
+        System.out.println("- Title Done: " +title);
+        Assert.assertTrue(title.contains("Build your own ring"));
 
         new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1/3 CT."); //drop-down list
         Select dropdown = new Select(driver.findElement(new By.ByName("size")));
         dropdown.selectByVisibleText("1/3 CT.");
 
-        Thread.sleep(5000);
+            Thread.sleep(5000);
 
-        ((JavascriptExecutor) driver).executeScript("arguments[0].styles.display = 'block';", driver.findElement(By.id("RD")));
-        driver.findElement(By.id("RD")).click();
+        dropdown = new Select(driver.findElement(new By.ByName("size")));
+        dropdown.selectByVisibleText("1/2 CT.");
 
-        Thread.sleep(5000);
+            Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("3,699.99")) {
+            System.out.println("- Cost Done");
+        }
+        else {
+            System.out.println("- Cost Fail");
+        }
+
+    }
+
+    @Test
+    public void Top13_XC2192K_OV_12() throws Exception { //Открыть ссылку и проверка на title
+
+        driver.get("https://zales.wonderjewel.co/configurator/XC2192K");
+        System.out.println("- Open url");
+
+        String title = driver.getTitle();
+        System.out.println("- Title Done: " + title);
+        Assert.assertTrue(title.contains("Build your own ring"));
+
+            Thread.sleep(5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Test
+    public void Top13_XC1160K_PC_13() throws Exception { //Открыть ссылку и проверка на title
+
+        driver.get("https://zales.wonderjewel.co/configurator/XC1160K");
+        System.out.println("- Open url");
+
+        String title = driver.getTitle();
+        System.out.println("- Title Done: " +title);
+        Assert.assertTrue(title.contains("Build your own ring"));
+
+            Thread.sleep(5000);
+
+        new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1/3 CT."); //drop-down list
+        Select dropdown = new Select(driver.findElement(new By.ByName("size")));
+        dropdown.selectByVisibleText("1/3 CT.");
+
+            Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("2,999.99")) {
+            System.out.println("- Cost Done");
+        }
+        else {
+            System.out.println("- Cost Fail");
+        }
+
+    }
+
+    @Test
+    public void Top13_XC1222K_PC_12() throws Exception { //Открыть ссылку и проверка на title
+
+        driver.get("https://zales.wonderjewel.co/configurator/XC1160K");
+        System.out.println("- Open url");
+
+        String title = driver.getTitle();
+        System.out.println("- Title Done: " +title);
+        Assert.assertTrue(title.contains("Build your own ring"));
+
+        new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1/3 CT."); //drop-down list
+        Select dropdown = new Select(driver.findElement(new By.ByName("size")));
+        dropdown.selectByVisibleText("1/3 CT.");
+
+            Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("2,999.99")) {
+            System.out.println("- Cost Done");
+        }
+        else {
+            System.out.println("- Cost Fail");
+        }
 
     }
 
@@ -63,10 +137,9 @@ public class Top13Specific extends WebDriver {
         WebDriverWait wait = new WebDriverWait(driver, 600);
         wait.until(ExpectedConditions.elementToBeClickable(By.id("RD")));
 
-        Thread.sleep(3000);
+            Thread.sleep(3000);
 
     }
-
 
 }
 
--- src/test/java/co/WJ/Trash.java
+++ src/test/java/co/WJ/Trash.java
@@ -1,9 +1,21 @@
-//        Assert.assertTrue(driver.findElement(By.cssSelector(".builder-radio-group")).getText().contains("Round"));
-//        System.out.println("- Title3");
+/*                  Assert.assertTrue(driver.findElement(By.cssSelector(".builder-radio-group")).getText().contains("Round"));
+                    System.out.println("- Title3");
 
-//        Assert.assertTrue(driver.findElement(By.className(".metal-type")).getText().contains("Gold"));
-//        System.out.println("- Title4");
+                    Assert.assertTrue(driver.findElement(By.className(".metal-type")).getText().contains("Gold"));
+                    System.out.println("- Title4");
 
-//        new Select(driver.findElement(By.id("diamond-size"))).selectByVisibleText("1/2 CT."); drop-down list
+                    new Select(driver.findElement(By.id("diamond-size"))).selectByVisibleText("1/2 CT."); drop-down list
 
 
+                    new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1/3 CT."); //drop-down list
+                    Select dropdown = new Select(driver.findElement(new By.ByName("size")));
+                    dropdown.selectByVisibleText("1/3 CT.");
+
+                    Thread.sleep(5000);
+
+                    dropdown = new Select(driver.findElement(new By.ByName("size")));
+                    dropdown.selectByVisibleText("1/2 CT.");
+
+                    driver.findElement(By.cssSelector("Body")).sendKeys(Keys.CONTROL + "t"); // открыть вкладку в новом окне
+
+ */
