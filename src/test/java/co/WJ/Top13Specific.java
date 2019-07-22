257bdb675455d837829f823a75aa272cd84910cc
Artem Pavelko <ma3au.pav@gmail.com>
2019-07-22 20:46:08 +0300

    id all

--- src/test/java/co/WJ/Top13Specific.java
+++ src/test/java/co/WJ/Top13Specific.java
@@ -4,25 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 
 public class Top13Specific extends WebDriver {
 
     private String count;
     private Object args;
+
 
     @Test
     public void Top13_XC2192K() throws Exception { //id 1,3
@@ -156,6 +151,7 @@ public class Top13Specific extends WebDriver {
 
     }
 
+
     @Test
     public void Top13_XC1160K() throws Exception { //id 8
 
@@ -207,7 +203,7 @@ public class Top13Specific extends WebDriver {
         dropdown = new Select(driver.findElement(new By.ByName("size")));
         dropdown.selectByVisibleText("1/2 CT.");
 
-        Thread.sleep(5000);
+        Thread.sleep(6000);
 
         if (driver.getPageSource().contains("3,999.99")) {
             System.out.println("- Cost Done (XC1222K 3,999.99)");
@@ -296,5 +292,126 @@ public class Top13Specific extends WebDriver {
 
     }
 
+
+    @Test
+    public void Top13_XC1090() throws Exception { //id 18
+
+        driver.get("https://zales.wonderjewel.co/configurator/XC1090");
+        System.out.println("- Open url");
+
+        String title = driver.getTitle();
+        System.out.println("- Title Done: " + title);
+        Assert.assertTrue(title.contains("Build your own ring"));
+
+        Thread.sleep(5000);
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].style.display = 'block';", driver.findElement(By.id("PE")));
+        driver.findElement(By.id("PE")).click();
+
+        Thread.sleep(5000);
+
+        new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1/3 CT."); //drop-down list
+        Select dropdown = new Select(driver.findElement(new By.ByName("size")));
+        dropdown.selectByVisibleText("1/3 CT.");
+
+        Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("2,499.99")) {
+            System.out.println("- Cost Done (XC1090 2,499.99)");
+        } else {
+            int number = new Random().nextInt(1000) + 5;
+
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot" + number + ".png"));
+
+            System.out.println("- Screenshot (XC1090 2,499.99)");
+        }
+
+    }
+
+
+    @Test
+    public void Top13_XC1061() throws Exception { //id 19,20,21,22
+
+        driver.get("https://zales.wonderjewel.co/configurator/XC1061");
+        System.out.println("- Open url");
+
+        String title = driver.getTitle();
+        System.out.println("- Title Done: " + title);
+        Assert.assertTrue(title.contains("Build your own ring"));
+
+        Thread.sleep(5000);
+
+        new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1 CT."); //drop-down list
+        Select dropdown = new Select(driver.findElement(new By.ByName("size")));
+        dropdown.selectByVisibleText("1 CT.");
+
+        Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("9,999.99")) {
+            System.out.println("- Cost Done (XC1061 9,999.99)");
+        } else {
+            int number = new Random().nextInt(1000) + 5;
+
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot" + number + ".png"));
+
+            System.out.println("- Screenshot (XC1061 9,999.99)");
+        }
+
+         dropdown = new Select(driver.findElement(new By.ByName("size")));
+         dropdown.selectByVisibleText("1/2 CT.");
+
+         Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("4,699.99")) {
+            System.out.println("- Cost Done (XC1061 4,699.99)");
+        } else {
+            int number = new Random().nextInt(1000) + 5;
+
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot" + number + ".png"));
+
+            System.out.println("- Screenshot (XC1061 4,699.99)");
+        }
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].style.display = 'block';", driver.findElement(By.id("RD")));
+        driver.findElement(By.id("RD")).click();
+
+        Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("4,699.99")) {
+            System.out.println("- Cost Done (XC1061 4,699.99)");
+        } else {
+            int number = new Random().nextInt(1000) + 5;
+
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot" + number + ".png"));
+
+            System.out.println("- Screenshot (XC1061 4,699.99)");
+        }
+
+        dropdown = new Select(driver.findElement(new By.ByName("size")));
+        dropdown.selectByVisibleText("1 CT.");
+
+        Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("9,999.99")) {
+            System.out.println("- Cost Done (XC1061 9,999.99)");
+        } else {
+            int number = new Random().nextInt(1000) + 5;
+
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot" + number + ".png"));
+
+            System.out.println("- Screenshot (XC1061 9,999.99)");
+        }
+
+    }
+
 }
+
+
+
+
 
