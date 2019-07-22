fe4ab105435b11bff262c8eb1fc99c9a9b0c57c6
Artem Pavelko <ma3au.pav@gmail.com>
2019-07-22 18:50:37 +0300

    Screenshot

--- src/test/java/co/WJ/Top13Specific.java
+++ src/test/java/co/WJ/Top13Specific.java
@@ -9,11 +9,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.File;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 
 public class Top13Specific extends WebDriver {
+
+    private String count;
+    private Object args;
 
     @Test
     public void Top13_XC2192K_MQ_12() throws Exception {
@@ -25,6 +34,8 @@ public class Top13Specific extends WebDriver {
         System.out.println("- Title Done: " +title);
         Assert.assertTrue(title.contains("Build your own ring"));
 
+        Thread.sleep(5000);
+
         new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1/3 CT."); //drop-down list
         Select dropdown = new Select(driver.findElement(new By.ByName("size")));
         dropdown.selectByVisibleText("1/3 CT.");
@@ -34,16 +45,22 @@ public class Top13Specific extends WebDriver {
         dropdown = new Select(driver.findElement(new By.ByName("size")));
         dropdown.selectByVisibleText("1/2 CT.");
 
-            Thread.sleep(5000);
+           Thread.sleep(5000);
 
-        if (driver.getPageSource().contains("3,699.99")) {
+        if (driver.getPageSource().contains("3,64499.99")) {
             System.out.println("- Cost Done");
         }
         else {
-            System.out.println("- Cost Fail");
+            int number = new Random().nextInt(1000)+5;
+
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot"+number+".png"));
+
+            System.out.println("- Screenshot");
         }
 
     }
+
 
     @Test
     public void Top13_XC2192K_OV_12() throws Exception { //Открыть ссылку и проверка на title
@@ -66,10 +83,49 @@ public class Top13Specific extends WebDriver {
             System.out.println("- Cost Done");
         }
         else {
-            System.out.println("- Cost Fail");
+            int number = new Random().nextInt(1000)+5;
+
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot"+number+".png"));
+
+            System.out.println("- Screenshot");
         }
 
     }
+
+
+    @Test
+    public void Top13_XC1035P_RD_12() throws Exception { //Открыть ссылку и проверка на title
+
+        driver.get("https://zales.wonderjewel.co/configurator/XC1035P");
+        System.out.println("- Open url");
+
+        String title = driver.getTitle();
+        System.out.println("- Title Done: " +title);
+        Assert.assertTrue(title.contains("Build your own ring"));
+
+        Thread.sleep(5000);
+
+        new Select(driver.findElement(new By.ByName("size"))).selectByVisibleText("1/3 CT."); //drop-down list
+        Select dropdown = new Select(driver.findElement(new By.ByName("size")));
+        dropdown.selectByVisibleText("1/3 CT.");
+
+        Thread.sleep(5000);
+
+        if (driver.getPageSource().contains("2,999.99")) {
+            System.out.println("- Cost Done");
+        }
+        else {
+            int number = new Random().nextInt(1000)+5;
+
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot"+number+".png"));
+
+            System.out.println("- Screenshot");
+        }
+
+    }
+
 
     @Test
     public void Top13_XC1160K_PC_13() throws Exception { //Открыть ссылку и проверка на title
@@ -93,10 +149,16 @@ public class Top13Specific extends WebDriver {
             System.out.println("- Cost Done");
         }
         else {
-            System.out.println("- Cost Fail");
+            int number = new Random().nextInt(1000)+5;
+
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot"+number+".png"));
+
+            System.out.println("- Screenshot");
         }
 
     }
+
 
     @Test
     public void Top13_XC1222K_PC_12() throws Exception { //Открыть ссылку и проверка на title
@@ -118,10 +180,16 @@ public class Top13Specific extends WebDriver {
             System.out.println("- Cost Done");
         }
         else {
-            System.out.println("- Cost Fail");
+            int number = new Random().nextInt(1000)+5;
+
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("C:\\screenshot\\screenshot"+number+".png"));
+
+            System.out.println("- Screenshot");
         }
 
     }
+
 
     @Test
     public void Top13_XC2192K_MQ_12d() throws Exception { //Открыть ссылку и проверка на title
